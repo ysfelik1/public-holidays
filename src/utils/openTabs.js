294ea6da200e01e5fd1aa2e196f6f59989daedda
yusufelik <ysfelik1@gmail.com>
2023-01-09 19:47:29 +0100

    added new function

---
+++ src/utils/openTabs.js
@@ -0,0 +1,21 @@
+ const openTab  = (evt, tabName) =>    {
+  // Declare all variables
+   
+
+  // Get all elements with class="tabcontent" and hide them
+  const tabContent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tab-links" and remove the class "active"
+  const tabLinks = document.getElementsByClassName("tab-links");
+  for (i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
